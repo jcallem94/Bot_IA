ec8fd8680ac293a0cd9dbbff110501a7c393b69f
jcallem94 <jcallem94@gmail.com>
2024-01-17 18:30:55 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+from weather_classification import weather_classification
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachments in  ctx.message.attachments:
+            file_name = attachments.filename
+            image_path = f"./image/{file_name}"
+            file_url = attachments.url
+            await attachments.save(f"./image/{file_name}")
+            result, final_result = weather_classification("keras_model.h5", "labels.txt",image_path)
+            for i in range(len(result)):
+                await ctx.send(result[i])
+
+            await ctx.send("Predicción final: ")
+            await ctx.send(final_result[0])
+            await ctx.send(final_result[1])
+
+    else:
+        await ctx.send("Olvidaste adjuntar la imagen :(")
+
+bot.run("MTEzODg2NzUwMzcwODI0MTkzMA.GguxCX.UW5m9B_Vcu_HQWtA1k0U6crOoBYQC61aQfUTZU")
